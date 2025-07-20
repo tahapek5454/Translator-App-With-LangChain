c8b85d034c5e951a068b510a23d33c62f35eb785
taha.pek <Taha.Pek@d-teknoloji.com.tr>
2025-07-20 15:32:11 +0300

    feat: add lang serve

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from simple_message import get_chain
+from fastapi import FastAPI
+from langserve import add_routes
 
+app = FastAPI(
+    title="Translation API",
+    description="An API for translating text using Azure OpenAI",
+    version="1.0.0"
+)
+chain = get_chain()
+
+add_routes(app, chain, path="/chain")
+
+# for langchain playground http://localhost:8000/chain/playground
 if __name__ == "__main__":
-    chain = get_chain()
-    response = chain.invoke({
-        "language": "English",
-        "text": "Ben yapay zeka ile çalışıyorum."
-    })
-    print(response)
+    import uvicorn
+    uvicorn.run(app, host="localhost", port=8000)
